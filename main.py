0b0ff6b23a0b61b7774ff2bf7542cd04297391d8
madhuvani1234 <44312077+madhuvani1234@users.noreply.github.com>
2023-07-17 14:59:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+madhu_turtle = turtle.Turtle()
+madhu_turtle.forward(100)
+madhu_turtle.right(90)
+madhu_turtle.forward(100)
+madhu_turtle.right(90)
+madhu_turtle.forward(100)
+madhu_turtle.right(90)
+madhu_turtle.forward(100)
+madhu_turtle.right(90)
