bb24fe16af5f6c8e969cae96d3cf9fb00784e48f
madhuvani1234 <44312077+madhuvani1234@users.noreply.github.com>
2023-07-17 15:50:45 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,28 @@
+#functions in python 
+
+import turtle
+madhu_turtle = turtle.Turtle()
+#square
+def square():
+  madhu_turtle.forward(100)
+  madhu_turtle.right(90)
+  madhu_turtle.forward(100)
+  madhu_turtle.right(90)
+  madhu_turtle.forward(100)
+  madhu_turtle.right(90)
+  madhu_turtle.forward(100)
+  madhu_turtle.right(90)
+  
+  
+  
+square()
+madhu_turtle.forward(200)
+square()
+
+
+
+
+
+
+  
+ 
