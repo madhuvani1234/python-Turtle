891a220689046379560e1d3cf0d572ff5940b5bd
madhuvani1234 <44312077+madhuvani1234@users.noreply.github.com>
2023-07-17 17:44:25 +0530

    Add files via upload

---
+++ Calendar module.py
@@ -0,0 +1,42 @@
+#The tool I'm using to write my Python code #online... https://repl.it.
+#calender module in python 
+import calendar
+import datetime
+import time
+#prints mon-sun 
+print(calendar.weekheader(3))
+print()
+#prints 1st week day in numeric form line 0,1..
+print(calendar.firstweekday())
+print()
+#print whole calender of that year and month
+print(calendar.month(2019,6))
+#matrix form
+print(calendar.monthcalendar(2019,6))
+#whole year
+print(calendar.calendar(2019))
+#prints day of the date which we give
+day_of_the_week= calendar.weekday(2023,7,17)
+print(day_of_the_week)
+#is leap
+print(calendar.isleap(2022))
+#how many leap years in b/w 2 years
+print(calendar.leapdays(2000,3000))
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+  
+ 
