55780cb0e2089cdde923a19d21281f8160aefaac
madhuvani1234 <44312077+madhuvani1234@users.noreply.github.com>
2023-07-17 15:11:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ This is the Template Repl for Python with Turtle.
 Python with Turtle lets you make graphics easily in Python.
 
 Check out the official docs here: https://docs.python.org/3/library/turtle.html
-"""
+
 Getting started with variable:
 Drawing a square:
+"""
 
 import turtle
 
